7351545ae086a151aa51d8f07aff6513275f287b
itsnouralawad <itsnouralawad@gmail.com>
2023-10-29 10:55:03 +0000

    gta4l : Set kernel CMD line

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,6 +41,22 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.f
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
 # Kernel
+BOARD_KERNEL_CMDLINE := \
+    console=ttyMSM0,115200n8 \
+    earlycon=msm_geni_serial,0x4a90000 \
+    androidboot.console=ttyMSM0 \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x237 \
+    firmware_class.path=/vendor/firmware_mnt/image \
+    service_locator.enable=1 \
+    swiotlb=2048 \
+    loop.max_part=7 \
+    video=vfb:640x400,bpp=32,memsize=3072000 \
+    androidboot.selinux=permissive \
+    audit=0
+
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_BASE := 0x00000000
